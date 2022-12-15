d75b5fbaf2d475bdc9171f0e22d36bd87c346114
anisha-04 <anisha.jadhav07@gmail.com>
2022-12-14 16:52:37 -0800

    LoginPage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, request, jsonify
+from flask import Flask, render_template, flash, redirect, request, jsonify,session,url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 # from flask_googlemaps import GoogleMaps
@@ -195,8 +195,8 @@ def displayForeCast():
   except:
     print('please try again')
     data = 'error'
-
-  return render_template('forecast.html',
+    
+    return render_template('forecast.html',
                          data=data,
                          img=img,
                          form=form,
@@ -247,6 +247,82 @@ def coordsRoute():
                          form=form,
                          current=getCurrentLocation(),
                          data=data, lat=lat,lon=lon)
+
+
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+ 
+  if request.method == "POST":
+      email = request.form.get("email")
+      password = request.form.get("password")
+      print(email,password)
+      conn = mysql.connect()
+      cur = conn.cursor()
+      cur.execute("select * from authentication where email=%s;",[email])
+      data = cur.fetchone()
+      cur.close()
+      conn.close()
+      if data != None:
+          if data[4] == password:
+              session["auth_id"] = data[0]
+              session["first_name"] = data[1]
+              session["last_name"] = data[2]
+              session["email"] = data[3]
+              return redirect(url_for("main"))
+          else:
+              session["error"] = "password doesn't match."
+              return redirect(url_for("login"))
+
+      else:
+          session["error"] = "user not exist."
+          return redirect(url_for("login"))
+  else:
+    return render_template("login.html")
+   
+        
+
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+  session.pop("first_name", None)
+  session.pop("last_name", None)
+  session.pop("email", None)
+  session.pop("auth_id", None)
+  return redirect(url_for("login"))
+
+
+
+
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+  if request.method == "POST":
+      first_name = request.form.get("first_name")
+      last_name = request.form.get("last_name")
+      email = request.form.get("email")
+      password = request.form.get("password")
+      conn = mysql.connect()
+      cur = conn.cursor()
+      cur.execute(""" select * from authentication where email=%s ; """,[email])
+      data = cur.fetchone()
+      if data == None:
+          cur.execute(''' insert into authentication (first_name,last_name,email,password) values(%s,%s,%s,%s);''',[first_name,last_name,email,password])
+          conn.commit()
+          cur.close()
+          conn.close()
+          return redirect("/home")
+      else:
+          session["error"] = "This Email Address Is Already Exist"
+          return redirect("/signup")
+  else:
+      user_name = session.get("user_name")
+
+      error = ""
+      if session.get("error"):
+          error = session.get("error")
+          session.pop("error", None)
+
+      return render_template("signupp.html",user_name=user_name,error=error)
 
 
 # @app.route("/map")
